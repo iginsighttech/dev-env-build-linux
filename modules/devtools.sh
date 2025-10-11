97d44ad0c2eaeb7a86c03e5d23fea09998168e90
iginsighttech <ismail.giles@itechcms.com>
2025-10-11 01:59:37 -0400

    v1.1 - fixed issues where tools don't install when script is run in live mode

--- modules/devtools.sh
+++ modules/devtools.sh
@@ -1,40 +1,49 @@
 #!/usr/bin/env bash
 # Developer tools installer module
-# Supports: pyenv, nvm, rbenv, git, jq
+# Supports: pyenv, nvm, rbenv, git, jq, powershell
 
 install_devtools() {
-    log_info "Installing developer tools..."
-    # Always install pyenv, nvm, rbenv in user mode
-    if [[ "${CONFIG_pyenv:-false}" == "true" ]]; then
-        if [[ "${INSTALL_MODE}" == "system" ]]; then
-            log_warn "pyenv should only be installed in user mode. Skipping system-wide install."
-        else
-            log_info "Would install pyenv (user mode)"
-        fi
-    fi
-    if [[ "${CONFIG_nvm:-false}" == "true" ]]; then
-        if [[ "${INSTALL_MODE}" == "system" ]]; then
-            log_warn "nvm should only be installed in user mode. Skipping system-wide install."
-        else
-            log_info "Would install nvm (user mode)"
-        fi
-    fi
-    if [[ "${CONFIG_rbenv:-false}" == "true" ]]; then
-        if [[ "${INSTALL_MODE}" == "system" ]]; then
-            log_warn "rbenv should only be installed in user mode. Skipping system-wide install."
-        else
-            log_info "Would install rbenv (user mode)"
-        fi
-    fi
-    if [[ "${CONFIG_git:-false}" == "true" ]]; then
-        log_info "Would install git"
-    fi
-    if [[ "${CONFIG_jq:-false}" == "true" ]]; then
-        log_info "Would install jq"
-    fi
-    if [[ "${CONFIG_powershell:-false}" == "true" ]]; then
-        log_info "Would install PowerShell"
-    fi
+    local tools="${1:-}" # comma-separated list
+    log_info "Installing developer tools: $tools"
+    IFS=',' read -ra tool_array <<< "$tools"
+    for tool in "${tool_array[@]}"; do
+        tool=$(trim "$tool")
+        case "$tool" in
+            pyenv)
+                if [[ "$INSTALL_MODE" == "system" ]]; then
+                    log_warn "pyenv should only be installed in user mode. Skipping system-wide install."
+                else
+                    log_info "Would install pyenv (user mode)"
+                fi
+                ;;
+            nvm)
+                if [[ "$INSTALL_MODE" == "system" ]]; then
+                    log_warn "nvm should only be installed in user mode. Skipping system-wide install."
+                else
+                    log_info "Would install nvm (user mode)"
+                fi
+                ;;
+            rbenv)
+                if [[ "$INSTALL_MODE" == "system" ]]; then
+                    log_warn "rbenv should only be installed in user mode. Skipping system-wide install."
+                else
+                    log_info "Would install rbenv (user mode)"
+                fi
+                ;;
+            git)
+                log_info "Would install git"
+                ;;
+            jq)
+                log_info "Would install jq"
+                ;;
+            powershell)
+                log_info "Would install PowerShell"
+                ;;
+            *)
+                log_warn "Unknown devtool: $tool"
+                ;;
+        esac
+    done
 }
 
 if [[ "${BASH_SOURCE[0]}" != "$0" ]]; then
